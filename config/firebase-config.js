ecb6d9cef2940095b4ad14c0e55ad38faff2eef6
bagusgandhi <bagusgandhi4@gmail.com>
2022-10-02 23:12:50 +0700

    env service account issue

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,16 +1,19 @@
 const admin = require('firebase-admin');
 
 // dotenv for development only
-// const dotenv = require('dotenv');
-// dotenv.config({ path: './.env' });
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 
 admin.initializeApp({
-		credential: admin.credential.cert({
-			project_id: process.env.project_id,
-			private_key: process.env.private_key,
-			client_email: process.env.client_email,
-	}),
+		credential: admin.credential.cert(serviceAccount)
+	// 	credential: admin.credential.cert({
+	// 		project_id: process.env.project_id,
+	// 		private_key: process.env.private_key,
+	// 		client_email: process.env.client_email,
+	// }),
 });
 
 module.exports = admin;
