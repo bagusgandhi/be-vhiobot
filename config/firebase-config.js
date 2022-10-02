89a1129e4e1861077e618c5c3e89f95f11bd30a3
bagusgandhi <bagusgandhi4@gmail.com>
2022-10-03 06:59:25 +0700

    fix issue env vars on production

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -6,14 +6,8 @@ const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-
 admin.initializeApp({
 		credential: admin.credential.cert(serviceAccount)
-	// 	credential: admin.credential.cert({
-	// 		project_id: process.env.project_id,
-	// 		private_key: process.env.private_key,
-	// 		client_email: process.env.client_email,
-	// }),
 });
 
 module.exports = admin;
--- utils/DialogFlow.js
+++ utils/DialogFlow.js
@@ -5,16 +5,18 @@ const uuid = require('uuid');
 // const dotenv = require('dotenv');
 // dotenv.config({ path: './.env' });
 
+const serviceAccount = JSON.parse(process.env.DIALOGFLOW_SERVICE_ACCOUNT)
+
 const sessionClient = new df.SessionsClient({
     credentials: {
-        private_key: process.env.private_key_df,
-        client_email: process.env.client_email_df,
+        private_key: serviceAccount.private_key,
+        client_email: serviceAccount.client_email,
     }
 });
 
 const queryText = async (message) => {
     const sessionid = uuid.v4();
-    const sessionPath = sessionClient.projectAgentSessionPath(process.env.project_id_df, sessionid);
+    const sessionPath = sessionClient.projectAgentSessionPath(serviceAccount.project_id, sessionid);
 
     const request = {
         session: sessionPath,
